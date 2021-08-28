d6e035eb7ace0134abf3d4378b96771d838b6a4b
rdawson24 <ryan.dawson@student.uts.edu.au>
2021-08-28 18:22:14 +1000

    Revert "Merge pull request #2 from rdawson24/revert-1-feature-print"
    This reverts commit 07d39ecb8e134b6ba1b60d589785d44faa47e70c, reversing
    changes made to a8ffe900f0b7b1321bc49034d2705dc137b37f2e.

---
+++ Assets/Scripts/PrintAndHide.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour
+{
+    public int i = 3;
+    public Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++;
+        Debug.Log(gameObject.name + ":" + i);
+    }
+}
